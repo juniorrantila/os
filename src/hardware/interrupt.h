9b0f1622d6f0f4764d315b285fb5ffde0dbcdfa2
Junior Rantila <junior.rantila@gmail.com>
2025-05-11 19:58:29 +0200

    hardware: add memory clobber for interrupt_enable and interrupt_disable

--- src/hardware/interrupt.h
+++ src/hardware/interrupt.h
@@ -1,18 +1,36 @@
 #pragma once
+#include <core/base.h>
 
 static inline void interrupt_disable(void)
 {
-    __asm__ __volatile__("cli");
+    __asm__ __volatile__("cli":::"memory");
 }
 
 static inline void interrupt_enable(void)
 {
-    __asm__ __volatile__("sti");
+    __asm__ __volatile__("sti":::"memory");
 }
 
 static inline void halt(void)
 {
     __asm__ __volatile__("hlt");
+}
+
+static inline u64 cpu_flags(void)
+{
+    u64 flags;
+    __asm__ __volatile__(
+        "pushf\n"
+        "pop %0\n"
+        : "=rm"(flags)
+        ::"memory"
+    );
+    return flags;
+}
+
+static inline bool are_interrupts_enabled(void)
+{
+    return (cpu_flags() & 0x200) != 0;
 }
 
 __attribute__((noreturn))
