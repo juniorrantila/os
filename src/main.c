e8801a8df344d231f69e9c4f8c1f6ac17171adae
Junior Rantila <junior.rantila@gmail.com>
2025-05-11 21:07:41 +0200

    main: parse commands before running them

--- src/main.c
+++ src/main.c
@@ -1,41 +1,65 @@
+#include <core/kprintf.h>
 #include <core/base.h>
 #include <core/string_view.h>
 #include <hardware/serial.h>
+#include <hardware/io.h>
+
+typedef enum {
+    Command_Count,
+    Command_Exit,
+    Command_Help,
+} Command;
+
+KSuccess parse_command(StringView, Command*);
 
 i32 main(i32 argc, c_string argv[])
 {
     (void)argc;
     (void)argv;
 
-    const StringView help = string_view("help");
-    const StringView exit = string_view("exit");
-    const StringView count = string_view("count");
-
     while (1) {
         kprintf("$ ");
         char buf[1024];
         u32 len = serial_readline(SERIAL_COM1, buf, sizeof(buf));
         StringView line = string_view_from_parts(buf, len);
-        if (string_view_equal(line, help)) {
-            kprintf("commands: help, exit, count\n");
+        Command command = { 0 };
+        if (!parse_command(line, &command).ok) {
+            kprintf("unknown command: '%.*s', see 'help'\n", line.count, line.items);
             continue;
         }
-
-        if (string_view_equal(line, exit)) {
+        switch (command) {
+        case Command_Help:
+            kprintf("commands: help, exit, count\n");
+            continue;
+        case Command_Exit:
             return 0;
-        }
-
-        if (string_view_equal(line, count)) {
+        case Command_Count:
             kprintf("starting to count\n");
-            for (int i = 0; i < 10; i++) {
+            for (i32 i = 0; i < 10; i++) {
                 kprintf("count: %d\n", i);
             }
             kprintf("done counting\n");
             continue;
         }
-
-        kprintf("unknown command: '%.*s', see 'help'\n", line.count, line.items);
     }
 
     return 0;
+}
+
+KSuccess parse_command(StringView command, Command* out)
+{
+    const StringView help = string_view("help");
+    const StringView exit = string_view("exit");
+    const StringView count = string_view("count");
+
+    if (string_view_equal(command, count))
+        return *out = Command_Count, ksuccess();
+
+    if (string_view_equal(command, exit))
+        return *out = Command_Exit, ksuccess();
+
+    if (string_view_equal(command, help))
+        return *out = Command_Help, ksuccess();
+
+    return kfailure();
 }
